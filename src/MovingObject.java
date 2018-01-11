66168f013a341436d0868d1b96a8bccb09611206
ETDavies <evandavies128@gmail.com>
2018-01-11 08:22:12 -0800

    Added MovingObject, Ship, and Projectile classes

---
+++ src/MovingObject.java
@@ -0,0 +1,10 @@
+
+public abstract class MovingObject {
+	private int speed, direction, distanceTravelled;
+	
+	public void move(int speed, int direction)
+	{}
+	
+	public void fadeAway()
+	{}
+}
---
+++ src/Ship.java
@@ -0,0 +1,22 @@
+
+public class Ship extends MovingObject{
+	private static int maxSpeed;
+	
+	void shoot(int direction)
+	{}
+	
+	void rotateClockwise()
+	{}
+	
+	void rotateCounterClockwise()
+	{}
+	
+	void accelerate()
+	{}
+	
+	void collision()
+	{}
+	
+	void respawn()
+	{}
+}
