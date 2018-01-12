31495eeb7525b2c1252b5ef268b3a3093dcda7b9
Cameron August <31994434+CJAuguzt@users.noreply.github.com>
2018-01-12 11:34:06 -0800

    Updated Board and GameDriver

--- src/Board.java
+++ src/Board.java
@@ -1,30 +1,62 @@
+
 /*V1 
+ * needs to use Java Nonblocking IO
  * CJA 1/11/2018
  */
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+
 //used for Board constructor
 //import java.io.*; 
 //import javax.imageio.*;
 //used for debugging
 public class Board extends JFrame {
-	ImageIcon icon;
+	static boolean inBoard = false;
+
 	public Board() {
-			setTitle("Asteroids");
-	        setResizable(true);
-	        icon = new ImageIcon("Images/Ship/S1V2.png");
-	        setIconImage(icon.getImage());
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        //Debugging
-	        //try {
-	        //setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("Images/menu/MainMenu1.png")))));
-	        //}catch(IOException e)
-	        //{
-	        //	System.out.println("Image Doesn't Exist");
-			//}
-	        setContentPane(new JLabel(new ImageIcon("Images/menu/MainMenu1.png")));
-	        setSize(1920,1080);
-	        
+		prepMenu();
+		inBoard = true;
+		// prepGame();
+	}
+	
+	private void prepMenu() {
+		setVisible(true);
+		setTitle("Asteroids");
+		setResizable(true);
+		ImageIcon icon = new ImageIcon("Images/Ship/S1V2.png");
+		setIconImage(icon.getImage());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Debugging
+		// try {
+		// setContentPane(new JLabel(new ImageIcon(ImageIO.read(new
+		// File("Images/menu/MainMenu1.png")))));
+		// }catch(IOException e)
+		// {
+		// System.out.println("Image Doesn't Exist");
+		// }
+		setContentPane(new JLabel(new ImageIcon("Images/menu/MainMenu1.png")));
+		setSize(1920, 1080);
+
+	}
+
+	private void prepGame() {
+		setVisible(true);
+		setTitle("Asteroids");
+		setResizable(true);
+		ImageIcon icon = new ImageIcon("Images/Ship/S1V2.png");
+		setIconImage(icon.getImage());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Debugging
+		// try {
+		// setContentPane(new JLabel(new ImageIcon(ImageIO.read(new
+		// File("Images/menu/MainMenu1.png")))));
+		// }catch(IOException e)
+		// {
+		// System.out.println("Image Doesn't Exist");
+		// }
+		setContentPane(new JLabel(new ImageIcon("Images/menu/Background.png")));
+		setSize(1920, 1080);
+
 	}
 }
--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,7 +1,44 @@
-
+import java.util.Scanner;
 public class GameDriver {
 	public static void main (String []args) {
+		//creates new instance of the Board to start game
 		Board menu = new Board();
-		menu.setVisible(true);
+		//looks to see if you are in the Main Menu
+		while (Board.inBoard == true) {
+		}
 	}
+	void rightPress(){
+		
+	}
+	void leftPress() {
+		
+	}
+	void spacePress() {
+		
+	}
+	void upPress() {
+		while (Board.inBoard) {
+			
+		}
+	}
+	void downPress() {
+		while (Board.inBoard) {
+		}
+	}
+	
+	int livesRemaining = 3; //starts off with 3 lives remaining 
+	void addLife () {
+		
+	}
+	
+	int wave = 1; //starts at wave 1, with set number of Asteroids to begin with
+	void startWave() {
+	}
+	
+	int score = 0;//starts with score of zero to be added onto
+	void ScoreUp() {
+		
+	}
+
 }
+
