5590072fb8e72f492aeb0f8969545af7e9cbd31b
N1ghtFallen <lukasumabat@gmail.com>
2018-01-16 08:19:40 -0800

    asteroid fixes

--- src/SmAsteroid.java
+++ src/SmAsteroid.java
@@ -1,6 +1,6 @@
 public class SmAsteroid extends MovingObject{
-	public SmAsteroid(float x, float y, float vx, float vy, float direction, boolean exists) {
-		super(x, y, vx, vy, direction, exists);
+	public SmAsteroid(float x, float y, float vx, float vy, float direction) {
+		super(x, y, vx, vy, direction);
 	}
 	public void Break() {
 		setExists(false);
