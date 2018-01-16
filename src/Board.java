f832da95441e45dc0c89c0b4344b1034367cde44
Cameron August <31994434+CJAuguzt@users.noreply.github.com>
2018-01-16 09:18:50 -0800

    THings

--- src/Board.java
+++ src/Board.java
@@ -6,40 +6,30 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.event.KeyEvent;
 
-//used for Board constructor
-//import java.io.*; 
-//import javax.imageio.*;
-//used for debugging
+
 public class Board extends JFrame {
-	static boolean inBoard = false;
+	static boolean inBoard;
+	static int keyValue;
 
 	public Board() {
 		prepMenu();
-		inBoard = true;
-		// prepGame();
+		if (inBoard == false) prepGame();
 	}
 	
-	private void prepMenu() {
+	public void prepMenu() {
+		inBoard = true;
 		setVisible(true);
 		setTitle("Asteroids");
 		setResizable(true);
 		ImageIcon icon = new ImageIcon("Images/Ship/S1V2.png");
 		setIconImage(icon.getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Debugging
-		// try {
-		// setContentPane(new JLabel(new ImageIcon(ImageIO.read(new
-		// File("Images/menu/MainMenu1.png")))));
-		// }catch(IOException e)
-		// {
-		// System.out.println("Image Doesn't Exist");
-		// }
 		setContentPane(new JLabel(new ImageIcon("Images/menu/MainMenu1.png")));
 		setSize(1920, 1080);
-
-	}
-
+		}
+	
 	private void prepGame() {
 		setVisible(true);
 		setTitle("Asteroids");
@@ -47,14 +37,6 @@ public class Board extends JFrame {
 		ImageIcon icon = new ImageIcon("Images/Ship/S1V2.png");
 		setIconImage(icon.getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Debugging
-		// try {
-		// setContentPane(new JLabel(new ImageIcon(ImageIO.read(new
-		// File("Images/menu/MainMenu1.png")))));
-		// }catch(IOException e)
-		// {
-		// System.out.println("Image Doesn't Exist");
-		// }
 		setContentPane(new JLabel(new ImageIcon("Images/menu/Background.png")));
 		setSize(1920, 1080);
 
--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,44 +1,60 @@
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
+
 public class GameDriver {
-	public static void main (String []args) {
-		//creates new instance of the Board to start game
+	public static int keyValue = 0;
+	public static void main(String[] args) {
+		// creates new instance of the Board to start game
 		Board menu = new Board();
-		//looks to see if you are in the Main Menu
+		// looks to see if you are in the Main Menu
 		while (Board.inBoard == true) {
-		}
+			KeyEvent e;
+			int key = e.getKeyCode();
+	        if (key == KeyEvent.VK_ENTER) {
+	        	keyValue = 1;
+	        }
+	        if (key == KeyEvent.VK_UP){
+	        	keyValue = 2;
+	        }
+	        if (key == KeyEvent.VK_DOWN) {
+	        	keyValue = 3;
+	        }
+	        
+			if (keyValue == 1) {
+				Board.inBoard = false;
+			}
+	      }
 	}
-	void rightPress(){
-		
-	}
-	void leftPress() {
-		
-	}
-	void spacePress() {
-		
-	}
-	void upPress() {
-		while (Board.inBoard) {
-			
-		}
-	}
-	void downPress() {
-		while (Board.inBoard) {
-		}
-	}
-	
+
+	/*public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_ENTER) {
+        	keyValue = 1;
+        }
+        if (key == KeyEvent.VK_UP){
+        	keyValue = 2;
+        }
+        if (key == KeyEvent.VK_DOWN) {
+        	keyValue = 3;
+        }
+	}*/
+
 	int livesRemaining = 3; //starts off with 3 lives remaining 
-	void addLife () {
-		
+
+	void addLife() {
+
 	}
-	
-	int wave = 1; //starts at wave 1, with set number of Asteroids to begin with
+
+	int wave = 1; // starts at wave 1, with set number of Asteroids to begin with
+
 	void startWave() {
 	}
-	
-	int score = 0;//starts with score of zero to be added onto
+
+	int score = 0;// starts with score of zero to be added onto
+
 	void ScoreUp() {
-		
+
 	}
 
 }
-
